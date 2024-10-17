c1326c9af4a97546e709d8538e10cf7fe6e15d8e
mraj <er.maanu@gmail.com>
2024-10-18 00:23:12 +0530

    modified classB

--- ClassB.java
+++ ClassB.java
@@ -3,5 +3,10 @@ public class ClassB {
     public static void main(String[] args)
     {
         System.out.println("added new classB");
+
+    }
+    public static void methoda()
+    {
+        System.out.println("methoda");
     }
 }
